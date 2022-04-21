e9a158406b680dccd5661379cf479320ee18d2be
UmairSprintX <umairamin@macs-MacBook-Pro.local>
2022-04-21 11:22:22 +0500

    start working on sleep

---
+++ app/screens/main/account/profile/sleep/sleep.tsx
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+type Props = {};
+
+const sleep = (props: Props) => {
+  return (
+    <View>
+      <Text>sleep</Text>
+    </View>
+  );
+};
+
+export default sleep;
