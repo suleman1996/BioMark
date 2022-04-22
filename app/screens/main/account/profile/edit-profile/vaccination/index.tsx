803cd0786586504bca77db4deb8ecdd79d3aab4a
Developer <Developer@work.com>
2022-04-22 14:20:20 +0500

    new

--- app/screens/main/account/profile/edit-profile/vaccination/index.tsx
+++ app/screens/main/account/profile/edit-profile/vaccination/index.tsx
@@ -14,36 +14,40 @@ import { styles } from './styles';
 export default function VaccinationScreen() {
 
   const [value, setValue] = useState('');
+  const [tags, setTags] = useState([]);
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <TitleWithBackLayout title="Vaccinations">
-        <Text style={styles.label}>Have you been vaccinated as an adult?</Text>
-        <RadioButton.Group
-          onValueChange={newValue => setValue(newValue)}
-          value={value}>
+        <ScrollView style={{ flex: 1, marginBottom: 100 }}>
+          <Text style={styles.label}>Have you been vaccinated as an adult?</Text>
+          <RadioButton.Group
+            onValueChange={newValue => setValue(newValue)}
+            value={value}>
 
-          <View style={[styles.radioContainer, { backgroundColor: value == 'first' ? '#054E8B' : null }]}>
-            <RadioButton color={value == 'first' ? GlobalColors.white : null} value="first" />
-            <Text style={[styles.radioText, { color: value == 'first' ? '#ffffff' : '#000000' }]}>No</Text>
-          </View>
+            <View style={[styles.radioContainer, { backgroundColor: value == 'first' ? '#054E8B' : null }]}>
+              <RadioButton color={value == 'first' ? GlobalColors.white : null} value="first" />
+              <Text style={[styles.radioText, { color: value == 'first' ? '#ffffff' : '#000000' }]}>No</Text>
+            </View>
 
-          <View style={[styles.radioContainer, { backgroundColor: value == 'second' ? '#054E8B' : null }]}>
-            <RadioButton color={value == 'second' ? GlobalColors.white : null} value="second" />
-            <Text style={[styles.radioText, { color: value == 'second' ? '#ffffff' : '#000000' }]}>Yes</Text>
-          </View>
+            <View style={[styles.radioContainer, { backgroundColor: value == 'second' ? '#054E8B' : null }]}>
+              <RadioButton color={value == 'second' ? GlobalColors.white : null} value="second" />
+              <Text style={[styles.radioText, { color: value == 'second' ? '#ffffff' : '#000000' }]}>Yes</Text>
+            </View>
 
-          <View style={[styles.radioContainer, { backgroundColor: value == 'third' ? '#054E8B' : null }]}>
-            <RadioButton color={value == 'third' ? GlobalColors.white : null} value="third" />
-            <Text style={[styles.radioText, { color: value == 'third' ? '#ffffff' : '#000000' }]}>Yes, but I'm not sure which vaccines</Text>
-          </View>
-        </RadioButton.Group>
+            <View style={[styles.radioContainer, { backgroundColor: value == 'third' ? '#054E8B' : null }]}>
+              <RadioButton color={value == 'third' ? GlobalColors.white : null} value="third" />
+              <Text style={[styles.radioText, { color: value == 'third' ? '#ffffff' : '#000000' }]}>Yes, but I'm not sure which vaccines</Text>
+            </View>
+          </RadioButton.Group>
 
-        {value == 'second' ?
-        <Text style={styles.label}>Please list the vaccines:</Text>
-        :null}
+          {value == 'second' ?
+            <Text style={styles.label}>Please list the vaccines:</Text>
+            : null}
 
+       
 
+        </ScrollView>
         <ButtonWithShadowContainer title='Save' />
       </TitleWithBackLayout>
     </SafeAreaView>
