233483566158d4d4fa5b451168ac63828b2b0e69
UmairSprintX <umairamin@macs-MacBook-Pro.local>
2022-04-23 13:34:22 +0500

    Qa bugs resolved

--- app/components/button/button.tsx
+++ app/components/button/button.tsx
@@ -1,6 +1,6 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import { Button } from 'react-native-paper';
+import {Button} from 'react-native-paper';
 import colors from '../../assets/colors/colors';
 
 type Props = {
@@ -24,10 +24,11 @@ export default function button(props: Props) {
         mode="contained"
         uppercase={false}
         disabled={props.disabled}
-        contentStyle={{ height: 50 }}
+        contentStyle={{height: 50}}
+        labelStyle={{color: colors.whiteColor}}
         style={[
           styles.btn,
-          { backgroundColor: props?.disabled ? '#8493AE60' : colors.blue },
+          {backgroundColor: props?.disabled ? '#8493AE60' : colors.blue},
         ]}
         onPress={props.onPress}>
         {props.title}
