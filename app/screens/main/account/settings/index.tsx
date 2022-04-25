8acdf4fc981092ed566e773cd45a27027622439f
codemili <junaidammar2013@gmail.com>
2022-04-26 04:31:22 +0500

    de activate account

--- app/screens/main/account/settings/index.tsx
+++ app/screens/main/account/settings/index.tsx
@@ -4,19 +4,47 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 import TitleWithBackWhiteBgLayout from 'components/layouts/back-with-title-white-bg';
 import SingleMenuItemWithArrow from 'components/higher-order/single-menu-item-with-right-arrow';
-import { navigate } from 'services/nav-ref';
+import { goBack, navigate } from 'services/nav-ref';
 import { Nav_Screens } from 'navigation/constants';
 import AccountDeActivateModal from 'components/ui/account-deactivate-modal';
+import { settingsService } from 'services/account-service/settings-service';
+import { ErrorResponse } from 'types/ErrorResponse';
+import { showMessage } from 'react-native-flash-message';
+import ActivityIndicator from 'components/loader/activity-indicator';
 
 const SettingsScreen = () => {
   const [isVisibleDeActivateModal, setDeActivateModal] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const disableAccountCall = () => {
+    setIsLoading(true);
+    settingsService
+      .deactivateAccount()
+      .then((res) => {
+        showMessage({
+          message: res.data.message,
+          type: 'success',
+        });
+        goBack();
+      })
+      .catch((err: ErrorResponse) => {
+        showMessage({
+          message: err.errMsg.data.message,
+          type: 'danger',
+        });
+      })
+      .finally(() => {
+        setDeActivateModal(false);
+        setIsLoading(false);
+      });
+  };
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <AccountDeActivateModal
-        callMe={() => setDeActivateModal(false)}
+        callMe={() => disableAccountCall()}
         isVisible={isVisibleDeActivateModal}
         setIsVisible={setDeActivateModal}
       />
+      <ActivityIndicator visible={isLoading} />
       <TitleWithBackWhiteBgLayout title="Settings">
         <SingleMenuItemWithArrow
           onPress={() => navigate(Nav_Screens.PasswordChangeScreen)}
