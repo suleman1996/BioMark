4a708a748387b1dff95aaa8d2c16e76d4f504bd2
Faizan Ali <faizan.ali@sprintx.net>
2022-05-13 14:37:00 +0500

    onboarding

--- app/screens/auth/login/index.tsx
+++ app/screens/auth/login/index.tsx
@@ -215,7 +215,6 @@ export default function Login() {
         <View style={{ height: 20 }} />
         <Text style={[styles.inputLablel, { marginTop: 20 }]}>Password</Text>
         <TextInput
-          // placeholder="Password"
           secureTextEntry={hidePassword}
           eye={hidePassword ? 'eye-off' : 'eye'}
           value={password}
