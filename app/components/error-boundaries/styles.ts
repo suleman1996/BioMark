8f342a6cd5bb3a14f843d0bbfd16c2cdac800bfc
Faizan Ali <faizan.ali@sprintx.net>
2022-05-17 13:23:26 +0500

    colors-errors-resolved

--- app/components/error-boundaries/styles.ts
+++ app/components/error-boundaries/styles.ts
@@ -1,34 +1,3 @@
-// import { StyleSheet } from 'react-native';
-
-// //import colors from 'assets/colors';
-// import fonts from 'assets/fonts';
-
-// export const styles = StyleSheet.create({
-//   container: {
-//     backgroundColor: colors.whiteColor,
-//     height: '100%',
-//     alignItems: 'center',
-//     paddingVertical: '20%',
-//   },
-
-//   icon: {
-//     alignSelf: 'center',
-//     marginBottom: 60,
-//   },
-//   heading: {
-//     fontFamily: fonts.bold,
-//     fontSize: 25,
-//     alignSelf: 'center',
-//     color: colors.blue,
-//     marginTop: 20,
-//     marginBottom: 25,
-//   },
-//   subHeading: {
-//     fontFamily: fonts.medium,
-//     fontSize: 16,
-//     lineHeight: 20,
-//   },
-// });
 import { StyleSheet } from 'react-native';
 
 //import colors from 'assets/colors';
@@ -50,7 +19,7 @@ export const styles = StyleSheet.create({
     fontFamily: fonts.bold,
     fontSize: 25,
     alignSelf: 'center',
-    color: 'blue',
+    color: '#1B96D8',
     marginTop: 20,
     marginBottom: 25,
   },
