44b069106eafb37af4f14e8669135a4ab3599f3c
Waqar Hussain <waqar.hussain@sprintx@.net>
2022-07-05 17:10:20 +0500

    fixes

--- app/components/show-upload-pic-modal/styles.ts
+++ app/components/show-upload-pic-modal/styles.ts
@@ -1,15 +1,16 @@
-import { StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
 import { heightToDp, widthToDp } from 'utils/functions/responsive-dimensions';
 
 const styles = () =>
   StyleSheet.create({
     // modal
     centeredView: {
-      flex: 1,
+      width: Dimensions.get('window').width,
+      height: Dimensions.get('window').height,
       justifyContent: 'center',
       alignItems: 'center',
-      marginTop: 22,
       backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      marginLeft: 0,
     },
     modalView: {
       backgroundColor: 'white',
