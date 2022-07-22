08f91fdc8c40c8d13cd01f56102af3b9af414522
Waqar Hussain <waqar.hussain@sprintx@.net>
2022-07-22 10:54:23 +0500

    fixes

---
+++ app/components/higher-order/search-bar-with-left-scan-icon/close-modal/styles.ts
@@ -0,0 +1,38 @@
+import fonts from 'assets/fonts';
+import { StyleSheet } from 'react-native';
+import { heightToDp, widthToDp } from 'utils/functions/responsive-dimensions';
+import { responsiveFontSize } from 'utils/functions/responsive-text';
+import { GlobalFonts } from 'utils/theme/fonts';
+
+export const makeStyles = (colors: any) =>
+  StyleSheet.create({
+    container: {
+      width: widthToDp(92),
+      backgroundColor: colors.white,
+      borderRadius: widthToDp(2),
+      paddingHorizontal: widthToDp(4),
+      paddingVertical: heightToDp(2),
+      alignItems: 'flex-start',
+    },
+    header: {
+      flexDirection: 'row',
+      width: '100%',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+    },
+    headerText: {
+      fontFamily: GlobalFonts.medium,
+      fontSize: responsiveFontSize(25),
+      color: colors.darkPrimary,
+    },
+    contentText: {
+      fontFamily: fonts.mulishLight,
+      fontSize: responsiveFontSize(18),
+      color: colors.darBlue,
+      lineHeight: responsiveFontSize(24),
+      marginTop: heightToDp(6),
+      marginBottom: heightToDp(6),
+    },
+  });
+
+export default makeStyles;
